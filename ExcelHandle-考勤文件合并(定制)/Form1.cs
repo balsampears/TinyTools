eb014b57b6c7f10f8377386b09ad8372472b791b
balsampears <798820407@qq.com>
2021-03-09 10:55:48 +0800

    添加“考勤合并”

---
+++ ExcelHandle-考勤文件合并(定制)/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelHandle.src;
+using NPOI;
+
+namespace ExcelHandle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string filenameDing;
+        private string filenameHe;
+
+
+        private void buttonImportDing_Click(object sender, EventArgs e)
+        {
+            openDialog(out filenameDing, buttonImportDing);
+        }
+
+        private void buttonImportHe_Click(object sender, EventArgs e)
+        {
+            openDialog(out filenameHe, buttonImportHe);
+        }
+
+        /// <summary>
+        /// 选择文件
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="button"></param>
+        private void openDialog(out string filename, Button button)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "请选择考勤表";
+            dialog.Filter = "表格文件|*.xls;*.xlsx";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = dialog.FileName;
+                button.Text = "已选择";
+                Console.WriteLine(filename);
+                Console.WriteLine(getFileSuffiex(filename));
+            } else
+            { 
+                filename = "";
+            }
+        }
+
+        /// <summary>
+        /// 获取文件后缀名
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        private string getFileSuffiex(string filename)
+        {
+            return filename.Substring(filename.IndexOf(".")+1);
+        }
+
+        /// <summary>
+        /// 开始生成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.filenameDing == null || this.filenameHe == null)
+            {
+                MessageBox.Show("请先选择文件！","错误警告");
+                return;
+            }
+            DataTable dtDing = NpoiHelper.Import(filenameDing, getFileSuffiex(filenameDing));
+            DataTable dtHe = NpoiHelper.Import(filenameHe, getFileSuffiex(filenameHe));
+
+            for (int i = 3; i < dtDing.Rows.Count; i++)
+            {
+                DataRow dr = dtDing.Rows[i];
+                this.RowHandleDingToHe(dtHe, dr);
+                for (int j = 0; j < dr.ItemArray.Length; j++)
+                {
+                    Console.Out.Write(j + "=>" + dr.ItemArray[j].ToString() + ",");
+                }
+                Console.Out.WriteLine();
+            }
+            //保存到桌面
+            saveFile(dtHe);
+        }
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        /// <param name="dtHe"></param>
+        private static void saveFile(DataTable dtHe)
+        {
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string saveFile = dir + "\\合并考勤表.xls";
+            NpoiHelper.Export(dtHe, "合并考勤表", saveFile);
+            MessageBox.Show("生成成功！文件已经保存在" + saveFile,"消息提示");
+            Console.Out.WriteLine("生成成功");
+        }
+
+        private void RowHandleDingToHe(DataTable dtHe, DataRow drDing)
+        {
+            string nothing = "暂无";
+            DataRow drHe = dtHe.NewRow();
+            drHe[Constants.He.Name] = drDing.ItemArray[Constants.Ding.Name];
+            //drHe[Constants.He.Quanqin] = nothing;
+            drHe[Constants.He.Chuqin] = GetAndCheckRowItem(drDing, Constants.Ding.Chuqin);
+            //drHe[Constants.He.Jiejiari] = nothing;
+            drHe[Constants.He.Waiqin] = GetAndCheckRowItem(drDing, Constants.Ding.Waiqin);
+
+            //drHe[Constants.He.Yichang] = nothing;
+            drHe[Constants.He.Chuchai] = GetAndCheckRowItem(drDing, Constants.Ding.Chuchai);
+            drHe[Constants.He.Shijia] = GetAndCheckRowItem(drDing, Constants.Ding.Shijia);
+            drHe[Constants.He.Nianjia] =GetAndCheckRowItem(drDing, Constants.Ding.Nianjia);
+            drHe[Constants.He.Bingjia] = GetAndCheckRowItem(drDing, Constants.Ding.Bingjia) ;
+
+            drHe[Constants.He.Hunjia] = GetAndCheckRowItem(drDing, Constants.Ding.Hunjia);
+            drHe[Constants.He.Shengyujia] =GetAndCheckRowItem(drDing, Constants.Ding.Shengyujia);
+            drHe[Constants.He.Huchanjia] = GetAndCheckRowItem(drDing, Constants.Ding.Huchanjia);
+            drHe[Constants.He.Furujia] = GetAndCheckRowItem(drDing, Constants.Ding.Furujia);
+            drHe[Constants.He.Sangjia] = GetAndCheckRowItem(drDing, Constants.Ding.Sangjia);
+
+            //drHe[Constants.He.Gongshangjia] = nothing;
+            //drHe[Constants.He.Liuchanjia] = nothing;
+            //drHe[Constants.He.Tanqinjia] = nothing;
+            drHe[Constants.He.Kuanggong] = GetAndCheckRowItem(drDing, Constants.Ding.Kuanggong);
+            drHe[Constants.He.Tiaoxiu] = GetAndCheckRowItem(drDing, Constants.Ding.Tiaoxiu);
+
+            drHe[Constants.He.Chidao5] = "右边的迟到累计时长";
+            drHe[Constants.He.Zaotui5] = GetAndCheckRowItem(drDing, Constants.Ding.Chidao);
+            drHe[Constants.He.Chidaoleiji] = GetAndCheckRowItem(drDing, Constants.Ding.Chidaoleiji);
+            drHe[Constants.He.Zaotuileiji] = GetAndCheckRowItem(drDing, Constants.Ding.Zaotuileiji);
+
+            drHe[Constants.He.Pingshijiaban] =GetAndCheckRowItem(drDing, Constants.Ding.Pingshijiaban);
+            drHe[Constants.He.Zhoumojiaban] = GetAndCheckRowItem(drDing, Constants.Ding.Zhoumojiaban);
+            drHe[Constants.He.Fadingjiejiari] =GetAndCheckRowItem(drDing, Constants.Ding.Fadingjiejiari);
+            drHe[Constants.He.Liudaitiaoxiu] = GetAndCheckRowItem(drDing, Constants.Ding.Liudaitiaoxiu);
+            dtHe.Rows.Add(drHe);
+        }
+        
+        /// <summary>
+        /// 获取一行
+        /// </summary>
+        /// <param name="drDing"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static object GetAndCheckRowItem(DataRow drDing, int index)
+        {
+            return drDing.ItemArray[index].ToString() != "" ? drDing.ItemArray[index] : 0;
+        }
+
+    }
+}
---
+++ ExcelHandle-考勤文件合并(定制)/src/Constants.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHandle.src
+{
+    public class Constants
+    {
+        public class He
+        {
+            public const int Name = 3;
+
+            public const int Quanqin = 5;
+            public const int Chuqin = 6;
+            public const int Jiejiari = 7;
+            public const int Waiqin = 8;
+            public const int Yichang = 9;
+
+            public const int Chuchai = 10;
+            public const int Shijia = 11;
+            public const int Nianjia = 12;
+            public const int Bingjia = 13;
+            public const int Hunjia = 14;
+
+            public const int Shengyujia = 15;
+            public const int Huchanjia = 16;
+            public const int Furujia = 17;
+            public const int Sangjia = 18;
+            public const int Gongshangjia = 19;
+
+            public const int Liuchanjia = 20;
+            public const int Tanqinjia = 21;
+            public const int Kuanggong = 22;
+            public const int Tiaoxiu = 23;
+            public const int Chidao5 = 24;
+
+            public const int Zaotui5 = 25;
+            public const int Chidaozaotui15 = 26;
+            public const int Chidaozaotui30 = 26;
+            public const int Chidaozaotui30x = 26;
+            public const int Chidaoleiji = 29;
+            public const int Zaotuileiji = 30;
+            public const int Pingshijiaban = 31;
+
+            public const int Zhoumojiaban = 32;
+            public const int Fadingjiejiari = 33;
+            public const int Liudaitiaoxiu = 34;
+        }
+        public class Ding
+        {
+            public const int Name = 0;
+
+            //public const int Quanqin = 6;
+            public const int Chuqin = 6;
+            //public const int Jiejiari = 8;
+            public const int Waiqin = 9;      //外出时长
+
+            //public const int Yichang = 10;
+            public const int Chuchai = 19;
+            public const int Shijia = 22;
+            public const int Nianjia = 21;
+            public const int Bingjia = 23;
+
+            public const int Hunjia = 27;
+            public const int Shengyujia = 25;
+            public const int Huchanjia = 26;
+            public const int Furujia = 28;
+            public const int Sangjia = 29;
+
+            //public const int Gongshangjia = 20;
+            //public const int Liuchanjia = 21;
+            //public const int Tanqinjia = 22;
+            public const int Kuanggong = 18;
+            public const int Tiaoxiu = 24;
+
+            public const int Chidao = 10; //记录迟到时长
+            //public const int Zaotui5 = 14;
+            //public const int Chidaozaotui15 = 26;
+            public const int Chidaoleiji = 9;
+            public const int Zaotuileiji = 14;
+
+            public const int Pingshijiaban = 31;
+            public const int Zhoumojiaban = 32;
+            public const int Fadingjiejiari = 33;
+            public const int Liudaitiaoxiu = 30;
+        }
+
+    }
+}
